6b49c943eb02ce898089a1b2042589574226f414
ZOhir12git <zohiroutabant@gmail.com>
2025-05-07 20:53:08 +0200

    Replace code in branch timer

--- calculte.h
+++
@@ -1,8 +0,0 @@
-#ifndef calculte_h
-#define calculte_h
-
-int add(int a , int b);
-int sub(int a , int b);
-int mult(int a , int b);
-int div(int a , int b);
-#endif
---
+++ time.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<time.h>
+int main(){
+    clock_t begin=clock();
+    printf("%d",begin);
+    int i;
+    for(i=0;i<100000000000;i++){
+
+    }
+    
+}
